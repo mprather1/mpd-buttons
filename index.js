208c43015b348ad4191e728468bfe95f63a4cfa3
Michael Prather <mprather1@gmail.com>
2017-11-06 14:40:09 -0600

    new

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const http = require('http')
+const execa = require('execa')
+const path = require('path')
+
+const app = express()
+const server = http.Server(app)
+const router = express.Router()
+
+server.on('listening', () => {
+  console.log('listening')
+})
+
+server.on('request', (req, res) => {
+  console.log(req.method, req.url, res.statusCode)
+})
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use('/css', express.static(path.join(__dirname, 'css')))
+app.use(router)
+
+router.route('/toggle')
+.get((req, res, next) => {
+  execa.shell('mpc toggle')
+  .then(result => {
+    res.status(200)
+    .send(result)
+  })
+})
+
+router.route('/next')
+.get((req, res, next) => {
+  execa.shell('mpc next')
+  .then(result => {
+    res.render('index', {
+      data: result.stdout.split('[')[0]
+    })
+  })
+})
+
+router.route('/prev')
+.get((req, res, next) => {
+  execa.shell('mpc prev')
+  .then(result => {
+    res.render('index', {
+      data: result.stdout.split('[')[0]
+    })
+  })
+})
+
+router.route('/')
+.get((req, res, next) => {
+  execa.shell('mpc')
+  .then(result => {
+    console.log(result)
+    res.render('index', {
+      data: result.stdout.split('[')[0]
+    })
+  })
+})
+
+server.listen(8000)
